221dcd837f755ced6f5ae8316f86f141a5436b15
cenfun <cenfun@qq.com>
2022-08-28 13:10:52 +0800

    fix id

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import decompress from 'lz-utils/lib/decompress.js';
 
-const getSvg = function(icon, size = '100%') {
-
-    //round
-    // <svg pointer-events="none" width="100%" height="100%">
-    //     <mask id="${fullId}-mask">
-    //         <rect rx="${radius}" ry="${radius}" fill="#ffffff" x="0" y="0" width="100%" height="100%" />
-    //     </mask>
-    //     <g mask="url(#${fullId}-mask)">
-    //         <rect rx="${radius}" ry="${radius}" fill="${color}" x="0" y="0" width="100%" height="100%" />
-    //         <use xlink:href="#${fullId}" />
-    //     </g>
-    // </svg>
+const initSvg = function(icon, size = '100%') {
 
     const list = ['<svg'];
     if (icon.viewBox) {
         list.push(` viewBox="${icon.viewBox}"`);
         delete icon.viewBox;
     }
+
     list.push(` width="${size}" height="${size}"`);
     if (icon.preserveAspectRatio) {
         list.push(` preserveAspectRatio="${icon.preserveAspectRatio}"`);
@@ -29,7 +19,24 @@ const getSvg = function(icon, size = '100%') {
     delete icon.content;
 
     list.push('</svg>');
-    return list.join('');
+
+    const svg = list.join('');
+    const prefixPlaceholder = '{prefix}';
+    const hasPrefix = svg.indexOf(prefixPlaceholder) !== -1;
+    let n = 0;
+    if (hasPrefix) {
+        //dynamic prefix
+        Object.defineProperty(icon, 'svg', {
+            get: function() {
+                n += 1;
+                const uid = `${icon.id}-${n}`;
+                return svg.split(prefixPlaceholder).join(uid);
+            }
+        });
+    } else {
+        icon.svg = svg;
+    }
+
 };
 
 const metadataStr = decompress('{placeholder_compressed_metadata}');
@@ -37,27 +44,25 @@ const metadata = JSON.parse(metadataStr);
 
 const icons = metadata.icons;
 
-//repeated content handler
+//init id and content
 icons.forEach((icon) => {
-    const id = `${metadata.id}-${icon.name}`;
-    icon.id = id;
-    //content and prefix handler
-    let content = icon.content;
+    icon.id = `${metadata.id}-${icon.name}`;
+
+    const content = icon.content;
     if (typeof content === 'number') {
-        content = icons[content].content;
+        //duplicate content
+        icon.content = icons[content].content;
     }
-    icon.content = content.split('{prefix}').join(id);
 });
 
-//generating svg
-icons.forEach(function(icon) {
-    icon.svg = getSvg(icon);
+icons.forEach((icon) => {
+    initSvg(icon);
 });
 
 export {
     metadata,
     decompress,
-    getSvg
+    initSvg
 };
 
 export default metadata;
